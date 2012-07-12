8dbf6a98110affcaecd17191c472a0f5915da0ea
lostindianredditor <jayanth.sunchu@gmail.com>
2012-07-12 11:42:28 -0500

    Update master

--- src/com/rsu/jayanthsunchu/redditsuitedup/ViewContextAdapter.java
+++ src/com/rsu/jayanthsunchu/redditsuitedup/ViewContextAdapter.java
@@ -1,5 +1,72 @@
 package com.rsu.jayanthsunchu.redditsuitedup;
 
-public class ViewContextAdapter {
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.rsu.jayanthsunchu.redditsuitedup.R;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class ViewContextAdapter extends BaseAdapter {
+  Activity context;
+	LayoutInflater mInflater;
+	ArrayList<HashMap<String, String>> returnList;
+
+	public ViewContextAdapter(Activity ctxt,
+			ArrayList<HashMap<String, String>> arrayList) {
+		this.context = ctxt;
+		this.returnList = arrayList;
+		this.mInflater = (LayoutInflater) ctxt
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return returnList.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return returnList.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		ViewHolder vh;
+		if (convertView == null) {
+			convertView = mInflater.inflate(R.layout.viewcontextlayout, null);
+			vh = new ViewHolder();
+			vh.txt = (TextView)convertView.findViewById(R.id.txtItem1);
+
+			convertView.setTag(vh);
+		} else {
+			vh = (ViewHolder) convertView.getTag();
+		}
+		if(returnList.size() > 0)
+		vh.txt.setText(returnList.get(position).get("name"));
+
+		return convertView;
+	}
+
+	public class ViewHolder {
+		TextView txt;
+
+	}
 
 }
